1b020b316bc7d062fa10a56a2d8739863566239d
nclong92 <nclong92@gmail.com>
2019-11-16 19:19:13 +0700

    Add project files.

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputConverter inputConverter = new InputConverter();
+            CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+            double firstNumber = inputConverter.ConvertInputToNumberic(Console.ReadLine());
+            string operation = Console.ReadLine();
+            double secondNumber = inputConverter.ConvertInputToNumberic(Console.ReadLine());
+            Console.WriteLine("=");
+
+            double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
