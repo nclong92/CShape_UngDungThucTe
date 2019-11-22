bc47e4fc21d12112a966d2ddf6eca6f829c1e5b9
longnc <nclong92@gmail.com>
2019-11-22 14:47:26 +0700

    WordUnscambler project

---
+++ WordUnscambler/Data/MatchedWord.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordUnscambler.Data
+{
+    public class MatchedWord
+    {
+        public string ScrambledWord { get; set; }
+        public string Word { get; set; }
+    }
+}
