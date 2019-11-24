2a0d40ba5ccac4ad79b7a916295bbe2160edb596
nclong92 <nclong92@gmail.com>
2019-11-24 11:43:34 +0700

    Sudoku Solver project

---
+++ SimpleWebScraper/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleWebScraper
+{
+    class Person
+    {
+        public string _ssn = "SSN";
+        public string _passportData = "Data";
+        public string _driversLicenseNumber = "More Data";
+
+        public bool HasProperDocuments
+        {
+            get
+            {
+                return _ssn.Length > 0 && _passportData.Length > 0 && _driversLicenseNumber.Length > 0;
+            }
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ SimpleWebScraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleWebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
